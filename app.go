e5b1238b993ef67645f5375223e766e7841e035b
Hunor Kovacs <hunor.kovacs@ft.com>
2017-11-21 17:07:43 +0200

    removed debug logging, i saw what's the problem: the checking for 13 hours back

--- app.go
+++ app.go
@@ -204,18 +204,15 @@ type scheduledJobResult struct {
 }
 
 func (m *mongobackup) backupScheduled(colls string, cronExpr string, dbPath string, run bool) error {
-	log.Infof("backupScheduled() entered")
 	err := os.MkdirAll(filepath.Dir(dbPath), 0600)
 
 	if err != nil {
 		return err
 	}
-	log.Infof("os.MkdirAll() called with success")
 	db, err := bolt.Open(dbPath, 0600, nil)
 	if err != nil {
 		return err
 	}
-	log.Infof("bolt.Open() called with success")
 	defer db.Close()
 
 	err = db.Update(func(tx *bolt.Tx) error {
@@ -223,19 +220,16 @@ func (m *mongobackup) backupScheduled(colls string, cronExpr string, dbPath stri
 		if err != nil {
 			return fmt.Errorf("create bucket: %s", err)
 		}
-		log.Infof("tx.CreateBucketIfNotExists() called with success")
 		return nil
 	})
 	if err != nil {
 		return err
 	}
-	log.Infof("db.Update() called with success")
 
 	parsed, err := parseCollections(colls)
 	if err != nil {
 		return err
 	}
-	log.Infof("parseCollections(colls) called with success")
 
 	c := cron.New()
 
@@ -243,7 +237,6 @@ func (m *mongobackup) backupScheduled(colls string, cronExpr string, dbPath stri
 		Name: "mongo-hot-backup-status",
 		Help: "Captures whether last backup was ok or not",
 	}, []string{"database", "collection"})
-	log.Infof("prometheus.NewGaugeVec() called with success")
 
 	var ids []scheduledJob
 
@@ -265,7 +258,6 @@ func (m *mongobackup) backupScheduled(colls string, cronExpr string, dbPath stri
 			} else {
 				metric.With(prometheus.Labels{"database": coll.database, "collection": coll.collection}).Set(1)
 			}
-			log.Infof("m.backup() called with success")
 
 			r, _ := json.Marshal(result)
 			db.Update(func(tx *bolt.Tx) error {
@@ -273,42 +265,14 @@ func (m *mongobackup) backupScheduled(colls string, cronExpr string, dbPath stri
 				err := b.Put([]byte(fmt.Sprintf("%s/%s", coll.database, coll.collection)), r)
 				return err
 			})
-			log.Infof("db.Update() after m.backup() called with success")
 		}
 
-		// on startup, we are registering status metrics to notify prom of the last backup status immediately
-		// we are also checking how long has it been since last backup, and if more than 13h we will trigger backup immediately
-		//err = db.View(func(tx *bolt.Tx) error {
-		//	b := tx.Bucket([]byte("Results"))
-		//	v := b.Get([]byte(fmt.Sprintf("%s/%s", coll.database, coll.collection)))
-		//
-		//	result := scheduledJobResult{}
-		//
-		//	json.Unmarshal(v, &result)
-		//
-		//	if !result.Success {
-		//		metric.With(prometheus.Labels{"database": coll.database, "collection": coll.collection}).Set(0)
-		//	} else {
-		//		metric.With(prometheus.Labels{"database": coll.database, "collection": coll.collection}).Set(1)
-		//	}
-
-			if run {
-				log.Infof("calling cronFunc()")
-				go cronFunc()
-			} else {
-				log.Infof("not calling cronFunc()")
-			}
-		//
-		//	return nil
-		//})
-		//if err != nil {
-		//	return err
-		//}
-		log.Infof("db.View() called with success")
+		if run {
+			go cronFunc()
+		}
 
 		eId, _ := c.AddFunc(cronExpr, func() { //now we add the cron methods
 
-			log.Infof("calling cronFunc()")
 			cronFunc()
 
 			for _, job := range ids {
