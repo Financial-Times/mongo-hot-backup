ca6177850540a99bcd1f73458eda8f5fb9a33a23
Hunor Kovacs <hunor.kovacs@ft.com>
2017-12-13 18:38:40 +0200

    New test in backupService.go

--- backupService_test.go
+++ backupService_test.go
@@ -1,37 +1,29 @@
 package main
 
-// import (
-// 	"io"
-// 	"testing"
-// 	"time"
+import (
+	"testing"
 
-// 	"github.com/stretchr/testify/assert"
-// 	"github.com/stretchr/testify/mock"
-// )
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
 
-// func TestBackup_Ok(t *testing.T) {
-// 	mockedMongoService := new(mockMongoService)
-// 	// stringWriter := bytes.NewBufferString("")
-// 	mockedMongoService.On("DialWithTimeout", "127.0.0.1:27010,127.0.0.2:27010", 0*time.Millisecond).Return(mockedMongoSession, nil)
+func TestBackup_Ok(t *testing.T) {
+	mockedStorageService := new(mockStorageServie)
+	mockedWriter := new(mockWriteCloser)
+	mockedWriter.On("Close").Return(nil)
+	mockedStorageService.On("Writer", mock.MatchedBy(func(date string) bool { return true }), "database1", "collection1").Return(mockedWriter, nil)
+	mockedMongoService := new(mockMongoService)
+	mockedMongoService.On("DumpCollectionTo", "database1", "collection1", mockedWriter).Return(nil)
+	mockedStatusKeeper := new(mockStatusKeeper)
+	mockedStatusKeeper.On("Save",
+		mock.MatchedBy(func(result backupResult) bool {
+			return result.Success &&
+				result.Collection.collection == "collection1" &&
+				result.Collection.database == "database1"
+		})).Return(nil)
+	backupService := newMongoBackupService(mockedMongoService, mockedStorageService, mockedStatusKeeper)
 
-// 	backupService := newMongoBackupService(mockedMongoService, "127.0.0.1:27010,127.0.0.2:27010", "s3bucket", "s3dir", "s3domain", "accessKey", "secretKey")
+	err := backupService.Backup([]dbColl{dbColl{"database1", "collection1"}})
 
-// 	err := backupService.Backup("s3dir", "database1", "collection1")
-
-// 	assert.NoError(t, err, "Error wasn't expected during backup.")
-// 	// assert.Equal(t, "datadatadata", stringWriter.String())
-// }
-
-// type mockMongoService struct {
-// 	mock.Mock
-// }
-
-// func (m *mockMongoService) DumpCollectionTo(connStr, database, collection string, writer io.Writer) error {
-// 	args := m.Called(connStr, database, collection, writer)
-// 	return args.Error(0)
-// }
-
-// func (m *mockMongoService) RestoreCollectionFrom(connStr, database, collection string, reader io.Reader) error {
-// 	args := m.Called(connStr, database, collection, reader)
-// 	return args.Error(0)
-// }
+	assert.NoError(t, err, "Error wasn't expected during backup.")
+}
--- testModels.go
+++ testModels.go
@@ -97,3 +97,59 @@ func (m *mockBsonService) ReadNextBSON(reader io.Reader) ([]byte, error) {
 	args := m.Called(reader)
 	return args.Get(0).([]byte), args.Error(1)
 }
+
+type mockMongoService struct {
+	mock.Mock
+}
+
+func (m *mockMongoService) DumpCollectionTo(database, collection string, writer io.Writer) error {
+	args := m.Called(database, collection, writer)
+	return args.Error(0)
+}
+
+func (m *mockMongoService) RestoreCollectionFrom(database, collection string, reader io.Reader) error {
+	args := m.Called(database, collection, reader)
+	return args.Error(0)
+}
+
+type mockStorageServie struct {
+	mock.Mock
+}
+
+func (m *mockStorageServie) Reader(date, database, collection string) (*snappyReadCloser, error) {
+	args := m.Called(date, database, collection)
+	return args.Get(0).(*snappyReadCloser), args.Error(1)
+}
+
+func (m *mockStorageServie) Writer(date, database, collection string) (io.WriteCloser, error) {
+	args := m.Called(date, database, collection)
+	return args.Get(0).(io.WriteCloser), args.Error(1)
+}
+
+type mockStatusKeeper struct {
+	mock.Mock
+}
+
+func (m *mockStatusKeeper) Save(result backupResult) error {
+	args := m.Called(result)
+	return args.Error(0)
+}
+
+func (m *mockStatusKeeper) Get(coll dbColl) (backupResult, error) {
+	args := m.Called(coll)
+	return args.Get(0).(backupResult), args.Error(1)
+}
+
+type mockWriteCloser struct {
+	mock.Mock
+}
+
+func (m *mockWriteCloser) Write(b []byte) (int, error) {
+	args := m.Called(b)
+	return args.Int(0), args.Error(1)
+}
+
+func (m *mockWriteCloser) Close() error {
+	args := m.Called()
+	return args.Error(0)
+}
